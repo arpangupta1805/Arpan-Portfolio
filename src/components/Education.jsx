f6e77e76ba82316acd01094caf3998d2f6a122ad
Arpan Gupta <24110051@iitgn.ac.in>
2025-01-11 18:18:57 +0530

    g

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -13,7 +13,7 @@ const Container = (delay) => ({
 
 const Education = () => {
   return (
-    <div className="flex flex-col items-center lg:items-start border-b border-neutral-900 pb-4">
+    <div className="flex flex-col items-center w-full lg:items-start border-b border-neutral-900 pb-4">
         <motion.span
             variants={Container(0.6)}
             initial="hidden"
@@ -25,15 +25,15 @@ const Education = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: 100 }}
         transition={{ duration: 0.8, delay: 0.5 }}
-        className='flex flex-wrap justify-start max-h-[30vh] max-w-full overflow-scroll'>
+        className='flex lg:flex-col gap-8 lg:flex-wrap justify-start max-h-[50vh] max-w-[95vw] scrollbar-hide overflow-scroll'>
         {EDUCATIONS.map((education, index) => (
-            <div key={index} className="w-full md:w-[30%] mb-8 flex-col flex-wrap flex lg:items-center border-[2px] border-gray-400 rounded-2xl p-4 ">
+            <div key={index} className="w-full md:w-[25rem] flex-col flex-wrap flex lg:items-center border-[2px] border-gray-600 rounded-2xl p-3 ">
                 <a target='_blank' href={education.link} className='cursor-pointer hover:underline'>
                   <motion.h1 
                       variants={Container(0)}
                       initial="hidden" // this is the initial state
                       animate="visible" // this is the final state
-                      className="text-2xl font-thin text-wrap tracking-tight lg:mt-16 lg:text-3xl" >{education.title}
+                      className="text-2xl font-thin text-wrap tracking-tight lg:mt-16" >{education.title}
                   </motion.h1>
                 </a>
                 <a target='_blank' href="https://cs.iitgn.ac.in" className='cursor-pointer transition-all hover:underline'>
@@ -41,7 +41,7 @@ const Education = () => {
                     variants={Container(0.5)}
                     initial="hidden"
                     animate="visible"
-                    className="my-1 ml-10 lg:ml-16 max-w-xl text-xl lg:text-2xl transition-all font-light tracking-tight">
+                    className="my-1 ml-10 lg:ml-16 max-w-xl text-xl transition-all font-light tracking-tight">
                     {education.subtitle}
                 </motion.h3>
                 </a>
@@ -49,7 +49,7 @@ const Education = () => {
                     variants={Container(0.7)}
                     initial="hidden"
                     animate="visible"
-                    className="my-1 ml-10 lg:ml-16 text-2xl max-w-xl font-light tracking-tight">
+                    className="my-1 ml-10 lg:ml-16 text-gray-500 text-2xl max-w-xl font-light tracking-tight">
                     {education.year}
                 </motion.p>
             </div>
